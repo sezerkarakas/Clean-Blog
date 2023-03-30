2baf2ab946530a4ce18824a73862ee53d01f61b3
sezerking <sezerking123@gmail.com>
2023-03-31 00:00:31 +0300

    template engine implemented

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-const express = require("express")
+const express = require('express');
+const ejs = require('ejs');
 
-const app = express()
+const app = express();
 
-app.get("/", (req, res) => {
+app.set('view engine', 'ejs');
 
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
+app.use(express.static('public'));
 
-    res.send(blog)
-    
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get("/post", (req,res) => {
+  res.render("post")
 })
-
-
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Sunucu port ${port}'de çalışıyor.`)
-})
+  console.log(`Sunucu port ${port}'de çalışıyor.`);
+});
