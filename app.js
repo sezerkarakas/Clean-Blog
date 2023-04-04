317ab993d72ee3ea017c3c8ebc86a84aaf121006
sezerking <sezerking123@gmail.com>
2023-04-04 16:01:10 +0300

    guncelleme

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const mongoose = require("mongoose")
 const ejs = require('ejs');
-
+const Post = require("./models/Posts")
 const app = express();
 
+mongoose.connect('mongodb://localhost/cleanblog-test-db',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+//TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
+//MIDDLEWARE
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  res.render('index', {
+    posts
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -21,6 +35,19 @@ app.get('/add_post', (req, res) => {
 
 app.get("/post", (req,res) => {
   res.render("post")
+})
+app.get("/admin", (req, res) => {
+  res.render("admin")
+})
+
+app.post("/posts", async (req, res) => {
+  try {
+    await Post.create(req.body)
+    res.redirect("/")
+  } catch (error) {
+    console.log(error)
+  }
+
 })
 
 const port = 3000;
