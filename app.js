04d92b44b9848c2dce87b428a198941b0fb529a5
sezerking <sezerking123@gmail.com>
2023-04-04 17:31:28 +0300

    Post page updated

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.get('/', async (req, res) => {
   });
 });
 
+app.get('/post/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id)
+  res.render("post",{
+    post
+  })
+});
 app.get('/about', (req, res) => {
   res.render('about');
 });
